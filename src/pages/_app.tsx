d5d18a79dfaa3904e9b0dc512e2de5578f907bc1
Stefanii Scurtu <sstefstefv@gmail.com>
2023-10-15 23:26:49 +0300

    fix: Ultra-Wide display

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "normalize.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@emotion/react";
-import { createTheme } from "@mui/material";
+import { StyledEngineProvider, createTheme } from "@mui/material";
 
 export default function App({ Component, pageProps }: AppProps) {
   const theme = createTheme({
@@ -16,8 +16,10 @@ export default function App({ Component, pageProps }: AppProps) {
   });
 
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
